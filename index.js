d9ece770d345ccaabfd34c00826d6d925ee27b9a
gotoqua <caiquech7@gmail.com>
2021-04-01 10:18:35 -0300

    formas diferentes de filtrar

--- index.js
+++ index.js
@@ -8,17 +8,36 @@ fs.readFile('./PessoasVacinacao.json', 'utf-8', (err, peopleString) => {
         const data = JSON.parse(peopleString);
 
         // Filtro do Grupo 1 - Maiores de 60 anos
-        const grupo1 = data.pessoas.filter(x=> x.idade>='60');
+        const grupo1 = data.pessoas
+        .filter(pessoa=> pessoa.idade>=60);
+        
         // console.log(grupo1);
 
         // Filtro do Grupo 2 - Profissionais das áreas de: Saúde, Educação Alimentícios e Segurança.
-        const grupo2 = data.pessoas.filter(x=> x.areaDeAtuacao==='Saúde' && x.idade<60 || x.areaDeAtuacao==='Segurança' && x.idade<60
-        || x.areaDeAtuacao==='Educação' && x.idade<60 || x.areaDeAtuacao==='Alimentícios' && x.idade<60);
-        console.log(grupo2);
-        // const checkAge = grupo2.filter(x=> x.idade<60);         
-        // console.log(checkAge);
+        let grupo2 = data.pessoas
+        .filter(pessoa => pessoa.idade < 60)
+        .filter(pessoa => pessoa.areaDeAtuacao==='Saúde' ||
+            pessoa.areaDeAtuacao === 'Segurança' ||
+            pessoa.areaDeAtuacao === 'Educação' ||
+            pessoa.areaDeAtuacao === 'Alimentícios')
 
-        // Filtro do Grupo 3 -
+        // console.log(grupo2);
+
+        // Filtro do Grupo 3 - 
+        const areasParaRemover = ['Saúde', 'Segurança', 'Educação', 'Alimentícios']
+
+        let filtrados = data.pessoas
+            .filter(pessoa => pessoa.idade < 60)
+            .filter(pessoa => !areasParaRemover.includes(pessoa.areaDeAtuacao))
+        
+        let filtrados2 = data.pessoas
+        .filter(pessoa => pessoa.idade < 60)
+        .filter(pessoa => pessoa.areaDeAtuacao !== 'Saúde' && 
+            pessoa.areaDeAtuacao !== 'Segurança' && 
+            pessoa.areaDeAtuacao !== 'Educação' && 
+            pessoa.areaDeAtuacao !== 'Alimentícios')
+
+         console.log(filtrados, filtrados2);
 
       } catch (err) {
           console.log('Error parsing JSON', err);
