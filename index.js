16271d700c785f2f26e032cd093ae0ad46442aac
gotoqua <caiquech7@gmail.com>
2021-03-31 22:00:18 -0300

    primeiro filtro criado

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 const fs = require('fs');
 
 fs.readFile('./PessoasVacinacao.json', 'utf-8', (err, peopleString) => {
-    console.log(peopleString);
-});
+    if (err) {
+        console.log(err);
+    } else {
+      try {
+        const data = JSON.parse(peopleString);
+        // Filtro do Grupo 1 - Maiores de 60 anos
+        const grupo1 = data.pessoas.filter(x=> x.idade>='60');
+        console.log(grupo1);
+        // Filtro do Grupo 2
+
+      } catch (err) {
+          console.log('Error parsing JSON', err);
+      }
+    }
+}); 
+
+
