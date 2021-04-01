9dad9d5ab63d02c44d3ad2f61828680131869625
gotoqua <caiquech7@gmail.com>
2021-04-01 07:53:31 -0300

    filtro do grupo 2 concluído

--- index.js
+++ index.js
@@ -6,12 +6,20 @@ fs.readFile('./PessoasVacinacao.json', 'utf-8', (err, peopleString) => {
     } else {
       try {
         const data = JSON.parse(peopleString);
+
         // Filtro do Grupo 1 - Maiores de 60 anos
         const grupo1 = data.pessoas.filter(x=> x.idade>='60');
         // console.log(grupo1);
-        // Filtro do Grupo 2
-        const grupo2 = data.pessoas.filter(x=> x.areaDeAtuacao==='Saúde' || x.areaDeAtuacao==='Segurança');
+
+        // Filtro do Grupo 2 - Profissionais das áreas de: Saúde, Educação Alimentícios e Segurança.
+        const grupo2 = data.pessoas.filter(x=> x.areaDeAtuacao==='Saúde' && x.idade<60 || x.areaDeAtuacao==='Segurança' && x.idade<60
+        || x.areaDeAtuacao==='Educação' && x.idade<60 || x.areaDeAtuacao==='Alimentícios' && x.idade<60);
         console.log(grupo2);
+        // const checkAge = grupo2.filter(x=> x.idade<60);         
+        // console.log(checkAge);
+
+        // Filtro do Grupo 3 -
+
       } catch (err) {
           console.log('Error parsing JSON', err);
       }
