fd614588b752ba3bebe57161aa2256f006c6c328
gotoqua <caiquech7@gmail.com>
2021-04-01 10:55:08 -0300

    finalizado!!!

--- index.js
+++ index.js
@@ -8,12 +8,16 @@ fs.readFile('./PessoasVacinacao.json', 'utf-8', (err, peopleString) => {
         const data = JSON.parse(peopleString);
 
         // Filtro do Grupo 1 - Maiores de 60 anos
+        console.log("Grupo 1 - Pessoas com idade à partir dos 60 anos")
+
         const grupo1 = data.pessoas
         .filter(pessoa=> pessoa.idade>=60);
         
-        // console.log(grupo1);
+        console.log(grupo1);
 
-        // Filtro do Grupo 2 - Profissionais das áreas de: Saúde, Educação Alimentícios e Segurança.
+        // Filtro do Grupo 2 - Profissionais das áreas de: Saúde, Educação, Alimentícios e Segurança.
+        console.log("Grupo 2 - Profissionais das áreas de: Saúde, Educação, Alimentícios e Segurança")
+
         let grupo2 = data.pessoas
         .filter(pessoa => pessoa.idade < 60)
         .filter(pessoa => pessoa.areaDeAtuacao==='Saúde' ||
@@ -21,23 +25,27 @@ fs.readFile('./PessoasVacinacao.json', 'utf-8', (err, peopleString) => {
             pessoa.areaDeAtuacao === 'Educação' ||
             pessoa.areaDeAtuacao === 'Alimentícios')
 
-        // console.log(grupo2);
+        console.log(grupo2);
 
-        // Filtro do Grupo 3 - 
+        // Filtro do Grupo 3 - Pessoas de 30 à 59 anos
+        console.log("Grupo 3 - Pessoas de 30 à 59 anos")
+
         const areasParaRemover = ['Saúde', 'Segurança', 'Educação', 'Alimentícios']
 
-        let filtrados = data.pessoas
-            .filter(pessoa => pessoa.idade < 60)
+        let grupo3 = data.pessoas
+            .filter(pessoa => pessoa.idade >= 30 && pessoa.idade < 60)
             .filter(pessoa => !areasParaRemover.includes(pessoa.areaDeAtuacao))
-        
-        let filtrados2 = data.pessoas
-        .filter(pessoa => pessoa.idade < 60)
-        .filter(pessoa => pessoa.areaDeAtuacao !== 'Saúde' && 
-            pessoa.areaDeAtuacao !== 'Segurança' && 
-            pessoa.areaDeAtuacao !== 'Educação' && 
-            pessoa.areaDeAtuacao !== 'Alimentícios')
 
-         console.log(filtrados, filtrados2);
+        console.log(grupo3);
+
+        // Filtro do Grupo 4 - Pessoas com idade até 30 anos
+        console.log("Grupo 4 - Pessoas com idade até 30 anos")
+
+        let grupo4 = data.pessoas
+            .filter(pessoa => pessoa.idade <= 30)
+            .filter(pessoa => !areasParaRemover.includes(pessoa.areaDeAtuacao))
+
+        console.log(grupo4);
 
       } catch (err) {
           console.log('Error parsing JSON', err);
